983b0102e0eb2bf9e4a32dcb55d3e2a7a985e7cf
Tommy Lee <taiynlee@gmail.com>
2024-04-24 14:00:24 +0800

    Update demo-pipeline.py

--- demo-pipeline.py
+++ demo-pipeline.py
@@ -1,57 +1,100 @@
-#  Copyright 2023 kbthu. All Rights Reserved.
+#!/usr/bin/env python3
+# Copyright 2020-2023 The Kubeflow Authors
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# %% [markdown]
+# # DSL control structures tutorial
+# Shows how to use conditional execution, loops, and exit handlers.
+
+# %%
 
 import kfp
 from kfp import dsl
-from kfp.components import func_to_container_op
-from typing import NamedTuple
 
 
-@func_to_container_op
-def calculate_op(a: float, b: float) -> NamedTuple(
-  'MyOutputs',
-  [
-    ('sum', float),
-    ('product', float)
-  ]):
-  return (a + b, a * b)
+@dsl.component
+def get_random_int_op(minimum: int, maximum: int) -> int:
+    """Generate a random number between minimum and maximum (inclusive)."""
+    import random
+    result = random.randint(minimum, maximum)
+    print(result)
+    return result
 
 
-def echo_func(sum: float, product: float):
-  cop = dsl.ContainerOp(
-    name='echo_func',
-    image='bash:5.1',
-    command=['sh', '-c'],
-    arguments=['echo sum %s product %s | tee /out.txt' % (sum, product)],
-	file_outputs={'out': '/out.txt'},
-  )
-  cop.container.set_image_pull_policy('IfNotPresent')
-  return cop
+@dsl.component
+def flip_coin_op() -> str:
+    """Flip a coin and output heads or tails randomly."""
+    import random
+    result = random.choice(['heads', 'tails'])
+    print(result)
+    return result
+
+
+@dsl.component
+def print_op(message: str):
+    """Print a message."""
+    print(message)
+
+
+@dsl.component
+def fail_op(message: str):
+    """Fails."""
+    import sys
+    print(message)
+    sys.exit(1)
+
+
+# %% [markdown]
+# ## Parallel execution
+# You can use the `with dsl.ParallelFor(task1.outputs) as items:` context to execute tasks in parallel
+
+# ## Conditional execution
+# You can use the `with dsl.Condition(task1.outputs["output_name"] = "value"):` context to execute parts of the pipeline conditionally
+
+# ## Exit handlers
+# You can use `with dsl.ExitHandler(exit_task):` context to execute a task when the rest of the pipeline finishes (succeeds or fails)
+
+# %%
 
 
 @dsl.pipeline(
-  name='Kubeflow data passing example',
-  description='Demonstrate the data passing between Kubeflow funcs'
+    name='tutorial-control-flows',
+    description='Shows how to use dsl.Condition(), dsl.ParallelFor, and dsl.ExitHandler().'
 )
-def data_passing(
-    a: dsl.PipelineParam(name='a', value=2), 
-    b: dsl.PipelineParam(name='b', value=3)
-  ):
-  result = calculate_op(a, b)
-  echo_func(result.outputs['sum'], result.outputs['product'])
+def control_flows_pipeline():
+    exit_task = print_op(message='Exit handler has worked!')
+    with dsl.ExitHandler(exit_task):
+        fail_op(
+            message="Failing the run to demonstrate that exit handler still gets executed."
+        )
+
+    flip = flip_coin_op()
+
+    with dsl.ParallelFor(['heads', 'tails']) as expected_result:
+
+        with dsl.Condition(flip.output == expected_result):
+            random_num_head = get_random_int_op(minimum=0, maximum=9)
+            with dsl.Condition(random_num_head.output > 5):
+                print_op(
+                    message=f'{expected_result} and {random_num_head.output} > 5!'
+                )
+            with dsl.Condition(random_num_head.output <= 5):
+                print_op(
+                    message=f'{expected_result} and {random_num_head.output} <= 5!'
+                )
 
 
 if __name__ == '__main__':
-  import kfp.compiler as compiler
-  compiler.Compiler().compile(data_passing, __file__ + '.yaml')
+    # Compiling the pipeline
+    kfp.compiler.Compiler().compile(control_flows_pipeline, __file__ + '.yaml')
